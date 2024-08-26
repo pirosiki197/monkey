426702431774a2cda3571fb0dca6302d090080b6
pirosiki197 <lingmukangtai825@gmail.com>
2024-08-26 23:05:01 +0900

    print asts

--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/pirosiki197/monkey/lexer"
-	"github.com/pirosiki197/monkey/token"
+	"github.com/pirosiki197/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -21,9 +22,20 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(os.Stdout, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		out.Write([]byte{'\n'})
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
