5973dbe5680ee51146c25c1203fc2ca8cb13dea3
pirosiki197 <lingmukangtai825@gmail.com>
2024-08-26 23:54:36 +0900

    add my first evaluator

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/pirosiki197/monkey/lexer"
+	"github.com/pirosiki197/monkey/object"
+	"github.com/pirosiki197/monkey/parser"
+)
+
+func TestEvalIntegers(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not %T. got=%T (%+v)", result, obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
